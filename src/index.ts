04c705985e60fca777aaf874169b0f377f943617
Cleyton <professor.cleyton.mach1@gmail.com>
2023-07-18 19:46:03 -0300

    resolvendo token

--- src/index.ts
+++ src/index.ts
@@ -9,154 +9,114 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json())
 
 type book = {
-    name: string
-    barcode?: number
-    publisherId?: number
-    price?: number
-    stock?: number
-    languageId?: number
+  name: string
+  barcode?: number
+  publisherId?: number
+  price?: number
+  stock?: number
+  languageId?: number
 }
 
 
 // Funcao para buscar todos os livros
 async function getBooks() {
-    const query = 'select * from livros';
-    const booksBD = await executeQuery(query, []);
-    return booksBD;
+  const query = 'select * from livros';
+  const booksBD = await executeQuery(query, []);
+  return booksBD;
 }
 
 async function getBookById(id: number) {
-    const query = 'select id, nome as name, preco as price, estoque as stock from livros where id=$1';
-    const params = [id];
-    const book = await executeQuery(query, params);
-    return book;
+  const query = 'select id, nome as name, preco as price, estoque as stock from livros where id=$1';
+  const params = [id];
+  const book = await executeQuery(query, params);
+  return book;
 }
 
 // Funcao para buscar todos os atendentes
 async function getUsers() {
-    const query = 'Select * from atendentes';
-    const usersDB = await executeQuery(query, []);
-    return usersDB;
+  const query = 'Select * from atendentes';
+  const usersDB = await executeQuery(query, []);
+  return usersDB;
 }
 
 async function insertBook(name: string, barcode: number, publisherId: number,
-    price: number, stock: number, languageId: number) {
-    const query = `INSERT INTO livros(
+  price: number, stock: number, languageId: number) {
+  const query = `INSERT INTO livros(
         nome, codigo_barras, id_editora, preco, 
         estoque, id_idioma)
         VALUES ($1, $2, $3, $4, $5, $6)`;
-    const params = [name, barcode, publisherId, price, stock, languageId];
-    executeQuery(query, params);
+  const params = [name, barcode, publisherId, price, stock, languageId];
+  executeQuery(query, params);
 }
 
 function isObjectEmpty(obj: object): boolean {
-    return Object.keys(obj).length === 0;
+  return Object.keys(obj).length === 0;
 }
-
-// async function updateBook(reqBody: any, id: number) {
-//     if (isObjectEmpty(reqBody)) {
-//         console.log('vazio')
-//         return false;
-//     }
-//     const { name, barcode, publisherId, price, stock, languageId } = reqBody;
-//     const params = [];
-//     params.push(id);
-//     let query = 'UPDATE livros SET '
-//     if (name) {
-//         query += 'nome=$2, '
-//         params.push(name)
-//     }
-//     if (barcode) {
-//         query += 'codigo_barras=$3, '
-//         params.push(barcode)
-//     }
-//     if (publisherId){
-//          query += 'id_editora=$4, '
-//         params.push(publisherId)
-//     }
-//     if (price) {
-//         query += 'preco=$5, '
-//         params.push(price)
-//     }
-//     if (stock) { 
-//         query += 'estoque=$6, '
-//         params.push(stock)
-//     }
-//     if (languageId) {
-//         query += 'id_idioma=$7, '
-//         params.push(languageId)
-//     }
-//     query = query.slice(0, -2)
-//     query += ' Where id=$1'
-
-//     console.log(query)
-//     executeQuery(query, params);
-// }
 
 // Melhora de atualizando o livro segundo GPT
 async function updateBook(reqBody: any, id: number) {
-    if (isObjectEmpty(reqBody)) {
-      console.log('vazio');
-      return false;
-    }
-    const { name, barcode, publisherId, price, stock, languageId } = reqBody;
-    const params = [id];
-    const updateFields = [];
-  
-    if (name) {
-      updateFields.push('nome=$2');
-      params.push(name);
-    }
-    if (barcode) {
-      updateFields.push('codigo_barras=$3');
-      params.push(barcode);
-    }
-    if (publisherId) {
-      updateFields.push('id_editora=$4');
-      params.push(publisherId);
-    }
-    if (price) {
-      updateFields.push('preco=$5');
-      params.push(price);
-    }
-    if (stock) {
-      updateFields.push('estoque=$6');
-      params.push(stock);
-    }
-    if (languageId) {
-      updateFields.push('id_idioma=$7');
-      params.push(languageId);
-    }
-  
-    const updateQuery = 'UPDATE livros SET ' + updateFields.join(', ') + ' WHERE id=$1';
-    console.log(updateQuery);
-    executeQuery(updateQuery, params);
+  if (isObjectEmpty(reqBody)) {
+    console.log('vazio');
+    return false;
   }
-  
+  const { name, barcode, publisherId, price, stock, languageId } = reqBody;
+  const params = [id];
+  const updateFields = [];
 
-app.get('/api/users', authMiddleware,async (req: Request, res: Response) => {
-    const users = await getUsers();
-    res.json(users)
+  if (name) {
+    updateFields.push('nome=$2');
+    params.push(name);
+  }
+  if (barcode) {
+    updateFields.push('codigo_barras=$3');
+    params.push(barcode);
+  }
+  if (publisherId) {
+    updateFields.push('id_editora=$4');
+    params.push(publisherId);
+  }
+  if (price) {
+    updateFields.push('preco=$5');
+    params.push(price);
+  }
+  if (stock) {
+    updateFields.push('estoque=$6');
+    params.push(stock);
+  }
+  if (languageId) {
+    updateFields.push('id_idioma=$7');
+    params.push(languageId);
+  }
+
+  const updateQuery = 'UPDATE livros SET ' + updateFields.join(', ') + ' WHERE id=$1';
+  console.log(updateQuery);
+  executeQuery(updateQuery, params);
+}
+
+
+app.get('/api/users', authMiddleware, async (req: Request, res: Response) => {
+  const users = await getUsers();
+  res.json(users)
 })
 
 // Buscar dados 
-app.get('/api/livros', async (req: Request, res: Response) => {
-    const books = await getBooks();
-    res.json(books);
+app.get('/api/livros', authMiddleware, async (req: Request, res: Response) => {
+  const books = await getBooks();
+  res.json(books);
 });
 
 // Buscar dados
 app.get('/api/livros/:id', async (req: Request, res: Response) => {
-    const bookId = Number(req.params.id);
-    const book = await getBookById(bookId);
-    res.json(book);
+  const bookId = Number(req.params.id);
+  const book = await getBookById(bookId);
+  res.json(book);
 });
 
 // Para criar livro
 app.post('/api/livros/cadastro', (req: Request, res: Response) => {
-    insertBook(req.body.name, req.body.barcode, req.body.publisherId,
-        req.body.price, req.body.stock, req.body.languageId)
-    res.json({ sucess: "Livro criado com sucesso!" });
+  insertBook(req.body.name, req.body.barcode, req.body.publisherId,
+    req.body.price, req.body.stock, req.body.languageId)
+  res.json({ sucess: "Livro criado com sucesso!" });
 });
 
 // Fazendo autenticacao 
@@ -174,17 +134,17 @@ app.post('/api/users/autentication', (req: Request, res: Response) => {
 
 // Atualizar um registro
 app.put('/api/livros/update/:id', (req: Request, res: Response) => {
-    const bookId = Number(req.params.id);
-    updateBook(req.body, bookId)
-    res.send(`Atualiza livro ${bookId}`);
+  const bookId = Number(req.params.id);
+  updateBook(req.body, bookId)
+  res.send(`Atualiza livro ${bookId}`);
 });
 
 // Remove registro
 app.delete('/api/livros/delete/:id', (req: Request, res: Response) => {
-    const bookId = req.params.id;
-    res.send(`Delete user with ID ${bookId}`);
+  const bookId = req.params.id;
+  res.send(`Delete user with ID ${bookId}`);
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log('Server is running on port 3000');
 });
